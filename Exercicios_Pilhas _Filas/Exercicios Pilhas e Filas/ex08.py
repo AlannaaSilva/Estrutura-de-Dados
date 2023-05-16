84b5dbb17e5a7ec432f5bdf8ef9df31365a79987
Alanna Silva <104801163+AlannaaSilva@users.noreply.github.com>
2023-05-16 08:38:47 -0300

    Add files via upload

---
+++ Exercicios_Pilhas _Filas/Exercicios Pilhas e Filas/ex08.py
@@ -0,0 +1,9 @@
+
+#Desenvolva um programa que seja capaz de cadastrar 5 elementos em uma lista.
+
+lista = []
+
+for i in range(5):
+    valor = input('Digite um valor')
+    lista.append(valor)
+print(lista)
---
+++ Exercicios_Pilhas _Filas/Exercicios Pilhas e Filas/ex09.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa que seja capaza de mostrar o último elemento de uma lista.
+
+
+lista = [1,2,3,4]
+
+ultimo = lista[-1]
+print(ultimo)
